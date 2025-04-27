21ad4fcf3112415656a385a74aa28f8b1c1bc282
AshenSankalpa <ashensankalpa6@gmail.com>
2025-04-27 16:49:34 +0530

    Functions

---
+++ funex.js
@@ -0,0 +1,29 @@
+//Functions
+function PrintMsg(){
+	console.log("Hello JS")
+}
+PrintMsg()
+
+function sum(){
+	return 5 + 6
+}
+console.log(sum())
+
+
+function sub(a, b){
+	return (a - b)
+}
+console.log(sub(5, 3))
+
+//Write a boolean function to find a given number is prime
+
+//Write recursive function to print numbers from 1 to n
+let num = 1
+let n = 10
+function PrintNum(){
+	num = num + 1
+	if(num == 1)
+	
+	
+	
+}
